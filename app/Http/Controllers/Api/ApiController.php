a3ddf8afc447d6f6ba12463229be7205c18c64c9
yaasiinaxmed <yaskassoy@gmail.com>
2025-09-13 07:05:49 +0300

    first commit

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    // Register API - name, email, password and password_confirmation
+    public function register(Request $request) {
+
+        $request->validate([
+            "name"=> "required|string",
+            "email"=> "required|email|unique:users,email",
+            "password"=> "required|confirmed",
+        ]);
+
+        User::create(attributes: $request->all());
+
+        return response()->json([
+            "status" => true,
+            "message"=> "User Registered Successfully"
+        ]);
+    }
+
+    // Login API
+    public function login(Request $request) {
+
+        $request->validate([
+            "name"=> "required|string",
+            "email"=> "required|email|unique:users,email",
+            "password"=> "required|confirmed",
+        ]);
+
+        // user check email
+        $user = User::Where("email", $request->email) ->first();
+
+        if ($user) {
+
+            // check password
+            if(Hash::check($request->password, $user->password)) {
+                $token = $user->createToken("myToken")->plainTextToken;
+
+                return response()->json([
+                    "status" => true,
+                    "message"=> "User Logged In Successfully",
+                    "token" => $token,
+                ]);
+            };
+        }
+    }
+
+    // Profile API
+    public function profile() {
+
+    }
+
+    // Logout API
+    public function logout() {
+
+    }
+    
+}
